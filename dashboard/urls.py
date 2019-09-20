2dd420f2edb72679935ed1944b3571ff444e7f21
krishna <krishnaprasad@goodbits.in>
2019-09-20 22:36:32 +0530

    dashboard-login

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,6 @@ from .views import *
 urlpatterns = [
     path('', Dashboard.as_view(), name='dashboard'),
     path('users', DashboardUsers.as_view(), name='dashboard-users'),
+    path('login', DashboardLogIn.as_view(), name='dashboard-login'),
+    path('logout', DashboardLogOut.as_view(), name='dashboard-logout')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.contrib.auth import login, logout
+from django.contrib import messages
 from django.views.generic import View, TemplateView
 from datetime import datetime
 from .models import *
+import copy
 
 
-class Dashboard(TemplateView):
+class DashboardLogIn(TemplateView):
+    template_name = 'dashboard/login.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def post(self, request):
+        """
+        :param request:
+        :username:Username
+        :password:Password
+        """
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        if username != "" and password != "":
+            try:
+                user = User.objects.get(username=username)
+                if user.is_superuser:
+                    if user.is_active:
+                        if user.check_password(password):
+                            login(request, user)
+                            return redirect(reverse('dashboard'))
+                        else:
+                            messages.error(request, "Invalid Credentials")
+                            return redirect(reverse('dashboard-login'))
+                    else:
+                        messages.error(request, "profile is not active, Please contact admin")
+                        return redirect(reverse('dashboard-login'))
+                else:
+                    messages.error("Invalid Credentials")
+            except User.DoesNotExist:
+                messages.error(request, "Invalid credentials, Please try again")
+                return redirect(reverse('dashboard-login'))
+        else:
+            messages.error(request, "Username and password is not valid")
+            return redirect(reverse('dashboard-login'))
+
+
+class DashboardLogOut(LoginRequiredMixin, View):
+    """
+    View for logging out user and redirect to login page
+    """
+    def get(self, request):
+        user = copy.deepcopy(request.user)
+        if request.user.is_authenticated:
+            logout(request)
+            messages.error(request, "You have been logged out. Hope you will be back soon.")
+        return redirect(reverse('dashboard-login'))
+
+
+class Dashboard(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard/dashboard.html'
 
     def get_context_data(self, *args, **kwargs):
@@ -27,7 +83,8 @@ class Dashboard(TemplateView):
         context['new_users'] = new_users
         return context
 
-class DashboardUsers(TemplateView):
+
+class DashboardUsers(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard/tables.html'
 
     def get_context_data(self, **kwargs):
@@ -35,3 +92,4 @@ class DashboardUsers(TemplateView):
         user_list = UserProfile.objects.all()
         context['user_list'] = user_list
         return context
+
