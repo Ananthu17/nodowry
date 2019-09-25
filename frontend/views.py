e0e1baf713750163be64e1c39662bb8c225518cc
krishna <krishnaprasad@goodbits.in>
2019-09-25 17:12:47 +0530

    ui chnages on forgot password and reset password screen

--- frontend/views.py
+++ frontend/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import View, TemplateView
 from dashboard.models import *
 from django.urls import reverse
+from datetime import date
 
 
 class HomePage(TemplateView):
@@ -20,16 +21,15 @@ class QuickFilter(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         gender = self.request.GET.get('gender')
-        agefrom = self.request.GET.get('agefrom')
-        ageto = self.request.GET.get('ageto')
+        agefrom = int(self.request.GET.get('agefrom'))
+        ageto = int(self.request.GET.get('ageto'))
         religion_id = self.request.GET.get('religion')
         language = self.request.GET.get('language')
-        print(gender)
-        print(agefrom)
-        print(ageto)
-        print(religion_id)
-        print(language)
-        userprofile = UserProfile.objects.filter(is_active=True, gender=gender, userinfo__religion=religion_id, userinfo__mother_tongue=language).values('user__first_name', 'userinfo__userimages__file', 'userinfo__dob')
+        currentdate = date.today()
+        print("today")
+        startdate = currentdate.replace(currentdate.year - agefrom)
+        enddate = currentdate.replace(currentdate.year - ageto)
+        userprofile = UserProfile.objects.filter(is_active=True, gender=gender, userinfo__religion=religion_id, userinfo__mother_tongue=language, userinfo__dob__range=(enddate, startdate)).values('user__first_name', 'userinfo__userimages__file', 'userinfo__dob')
         context['user_profile'] = userprofile
         context['user_count'] = userprofile.count()
         return context
